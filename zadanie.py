aa800ddbde2fa32ff70381ac24f0925b237691b7
Kacper Lechański <klechanski@edu.cdv.pl>
2020-05-12 00:24:31 +0200

    Losowanie liczb całkowitych z zakresu

--- zadanie.py
+++ zadanie.py
@@ -1,4 +1,16 @@
-print("Podaj wartość a: ")
-a=input()
-print("Podaj wartość b: ")
-a=input()
+
+#IMPORTUJEMY BIBLIOTEKĘ 'RANDOM'
+import random
+#IMPORTUJEMY FUNKCJĘ 'SHUFFLE()'
+from random import shuffle
+
+#WPISUJEMY ZAKRES LOSOWANYCH LICZB ORAZ ILOŚĆ LICZB DO LOSOWANIA
+a=int(input("Podaj wartość minimalną: "))
+b=int(input("Podaj wartość maksymalną: "))
+c=int(input("Podaj ilość liczb do wylosowania: "))
+
+#PĘTLA FOR (start, stop, krok):
+for i in range (0, c, 1):
+    #PRZYPISUJEMY WYLOSOWANĄ LICZBĘ CAŁKOWITĄ Z ZAKRESU DO ZMIENNEJ
+    los = random.randrange(a, b, 1)#randrange - losuje liczby całkowite z zakresu 'a->b' ze skokiem co 1
+    print("Liczba ",i + 1,": ", los)
