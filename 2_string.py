5813428ae17bcb9906c41c48851f3a4d04426399
Kacper Lechański <klechanski@edu.cdv.pl>
2020-06-02 01:21:34 +0200

    2_string - closed

--- 2_string.py
+++ 2_string.py
@@ -10,3 +10,37 @@ print(text)
 print(tab)
 
 print(type(tab))
+
+name = "JANUSZ"
+print(name)
+nameLower = name.lower()
+print(nameLower)
+
+surname=input("Podaj swoje nazwisko: ")
+content=surname.isalpha()
+print(content)
+
+text1="Jan\n"
+text2="Nowak"
+print(text1+text2)
+
+text1=text1.rstrip()
+print(text1 + ' ' + text2)
+
+#wyświetlanie łańcuchów znaków
+text = "%s, Java i %s" % ("PHP", "Python")
+print(text)
+
+text = "{1}, Java i {0}".format("PHP", "Python")
+print(text)
+
+help(text.replace)
+
+new = text.replace("PHP", "C#")
+print(new)
+
+year=2020
+month='may'
+day=24
+print("Data: ", end="")
+print(day, month, year, sep="-")
