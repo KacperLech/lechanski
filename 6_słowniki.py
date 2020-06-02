58e1d706513917559dfd51607cf54e1363ce5852
Kacper Lechański <klechanski@edu.cdv.pl>
2020-06-02 22:50:32 +0200

    słowniki

---
+++ 6_słowniki.py
@@ -0,0 +1,11 @@
+dict={'key1':'value1','key2':'value2'}
+'''
+Utwórz słownik o nazwie pracownik zawierający klucze:
+imie, nazwisko, miasto, wiek, imionaDzieci (podaj dwa imiona), imionaRodzicow(podaj dwa imiona rodzicow)
+Proszę pojedynczo wyświetlić imiona dzieci:
+Dziecko 1: ....
+Dziecko 2: ....
+'''
+
+pracownik = {'imie':'Kacper', 'nazwisko':'Lechański', 'miasto':'Poznań', 'wiek':'23', 'imionaDzieci':'Mateusz, Ania', 'imionaRodzicow': 'Edyta, Dariusz'}
+print(pracownik)
