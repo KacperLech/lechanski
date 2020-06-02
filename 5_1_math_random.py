c069b8de366f92dce33896d22af085ada03e5352
Kacper Lechański <klechanski@edu.cdv.pl>
2020-06-02 22:41:01 +0200

    5_1_math random - closed

--- 5_1_math_random.py
+++ 5_1_math_random.py
@@ -1,3 +1,16 @@
 #alias
 import math as m
 print(m.pow(2,4))
+
+import random as r
+rand=r.random()
+print(rand)
+
+print()
+print(r.randrange(10))
+print(r.randrange(10, 20))
+print(r.randrange(9, 13, 2))
+
+numList = [1, 2, 3, 4, 5]
+r.shuffle(numList)
+print(numList)
