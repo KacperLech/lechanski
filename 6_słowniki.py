7e24d30ed1d0e98476bd72c22a8b0e7cde5b6147
Kacper Lechański <klechanski@edu.cdv.pl>
2020-06-04 21:50:01 +0200

    słowniki

--- 6_słowniki.py
+++ 6_słowniki.py
@@ -1,11 +1,20 @@
 dict={'key1':'value1','key2':'value2'}
 '''
 Utwórz słownik o nazwie pracownik zawierający klucze:
-imie, nazwisko, miasto, wiek, imionaDzieci (podaj dwa imiona), imionaRodzicow(podaj dwa imiona rodzicow)
+imie, nazwisko, miasto, wiek, imionaDzieci (podaj dwa imiona - za pomocą listy), imionaRodzicow(podaj dwa imiona rodzicow - za pomocą listy)
 Proszę pojedynczo wyświetlić imiona dzieci:
 Dziecko 1: ....
 Dziecko 2: ....
 '''
-
-pracownik = {'imie':'Kacper', 'nazwisko':'Lechański', 'miasto':'Poznań', 'wiek':'23', 'imionaDzieci':'Mateusz, Ania', 'imionaRodzicow': 'Edyta, Dariusz'}
-print(pracownik)
+worker = {
+    'firstName':'Kacper',
+    'lastName':'Lechański',
+    'city':'Poznań',
+    'age':'23',
+    'namesChildren':['Marcin', 'Ania'],
+    'namesParent':['Edyta', 'Dariusz']
+}
+print(worker)
+print(worker['namesChildren'])
+print('Dziecko 1: ' + worker['namesChildren'][0])
+print('Dziecko 2: ' + worker['namesChildren'][1])
